09ea81e36dbab593e0a5e98843653d09efd1b819
Logan <fivepointbro@gmail.com>
2018-05-15 16:55:15 -0300

    alrighty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateService } from '@ngx-translate/core';
 import { Config, Nav, Platform } from 'ionic-angular';
-
+import { SocialSharing } from '@ionic-native/social-sharing';
 import { FirstRunPage } from '../pages';
 import { Settings } from '../providers';
 
@@ -36,7 +36,7 @@ export class MyApp {
     { title: 'Home', component: 'TabsPage' },
   ]
 
-  constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+  constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen, public socialSharing: SocialSharing) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
